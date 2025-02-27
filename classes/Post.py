970a764d1166e6294e8305d717cb01c5ba7fd50f
noam tsruya <zruyianoam@gmail.com>
2025-02-27 19:08:01 +0200

    added add_comment

--- classes/Post.py
+++ classes/Post.py
@@ -67,3 +67,15 @@ class Post:
             position_index += 1
             if i >= NUM_OF_COMMENTS_TO_DISPLAY - 1:
                 break
+
+
+class ImagePost(Post):
+        def __Init__(self,username,location,description,image_post):
+            super().__init__(username,location,description)
+            self.image=pygame.image.load(image_path)
+            self.image=pygame.transform.scale(self.image,(POST_WIDTH,POST_HEIGHT))
+
+
+        def display(self):
+            screen.blit(self.image,(POST_X_POS,POST_Y_POS))
+            super().display()
